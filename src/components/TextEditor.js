40e2cc069ca7c0bf5b884adf9076fa932e5edef0
Andriy Prodan <prodandgri@gmail.com>
2021-06-09 15:08:43 +0300

    Add buttons for editing TextEditor content
    Posibility to add bold and italic text and hyperlinks using buttons in panel

--- src/components/TextEditor.js
+++ src/components/TextEditor.js
@@ -3,43 +3,90 @@ import ReactMarkdown from 'react-markdown';
 import remarkMath from 'remark-math'
 import rehypeKatex from 'rehype-katex'
 import 'katex/dist/katex.min.css'
-import ReactDOM from 'react-dom';
 
 import './TextEditor.css';
 
+const PROTOCOLS = ['https://', 'http://']
+
 function TextEditor(props) {
     const [content, setContent] = useState('');
-    // Selection of the text inside the ContentEditable
-    const [range, setRange] = useState(null);
+    const [selection, setSelection] = useState({selectTextOnComponentUpdate: false});
+
+    useEffect(() => {
+        if (selection.selectTextOnComponentUpdate) {
+            let textArea = document.getElementById("markdown-textarea");
+            textArea.focus();
+            textArea.setSelectionRange(selection.start, selection.end);
+            textArea.selectionEnd = selection.end;
+            setSelection({
+                ...selection,
+                selectTextOnComponentUpdate: false,
+            })
+        }
+    }, [selection]);
 
     function handleContentChange(e) {
         setContent(e.target.value);
     }
 
-    function restoreSelection() {
-        const selection = window.getSelection();
-        selection.removeAllRanges();
-        selection.addRange(range);
-        return selection;
-    }
+    function wrapSelectedText(openTag, closeTag, placeholder='', selectWrappedText = true) {
+        let textArea = document.getElementById("markdown-textarea");
+        let len = textArea.value.length;
+        let start = textArea.selectionStart;
+        let end = textArea.selectionEnd;
 
-    function handleTextareaMouseUp() {
-        setRange(window.getSelection().getRangeAt(0));
-    }
+        // check if the selected text is already wrapped into that Tags
+        if (
+            textArea.value.substring(start - openTag.length, start) === openTag &&
+            textArea.value.substring(end, end + closeTag.length) === closeTag
+        ) {
+            // remove tags
+            let arr = textArea.value.split('');
+            arr.splice(start - openTag.length, openTag.length, '');
+            // subtract openTag.length - 1, because length of array is decreased by this number
+            // -1, because an empty string('') is added to array after splice
+            arr.splice(end - (openTag.length - 1), closeTag.length, '');
+            setContent(arr.join(''));
+            return;
+        }
 
-    function wrapText(openTag, closeTag) {
-        var textArea = document.getElementById("markdown-textarea");
-        var len = textArea.value.length;
-        var start = textArea.selectionStart;
-        var end = textArea.selectionEnd;
-        var selectedText = textArea.value.substring(start, end);
-        var replacement = openTag + selectedText + closeTag;
+        let selectedText = textArea.value.substring(start, end);
+        // If there is no selected text (selectionStart and selectionEnd are the same) placeholder is used
+        let text = start === end ? placeholder : selectedText
+        let replacement = openTag + text + closeTag;
         let new_content = textArea.value.substring(0, start) + replacement + textArea.value.substring(end, len);
         setContent(new_content);
+        
+        if (selectWrappedText) {
+            setSelection({
+                selectTextOnComponentUpdate: true,
+                start: start + openTag.length,
+                end: start + openTag.length + text.length
+            })
+        }
     }
 
     function handleBoldIconClick() {
-        wrapText("**", "**");
+        wrapSelectedText("**", "**", 'Strong text');
+    }
+
+    function handleItalicIconClick() {
+        wrapSelectedText("*", "*", 'Italic text');
+    }
+
+    function handleHyperlinkIconClick() {
+        let textArea = document.getElementById("markdown-textarea");
+        let selectedText = textArea.value.substring(textArea.selectionStart, textArea.selectionEnd);
+
+        let descriptionPlaceholder = 'Link description';
+        let linkPlaceholder = 'www.example.com'
+
+        let protocol = '';
+        if (!PROTOCOLS.some(p => selectedText.includes(p))) {
+            protocol = 'https://';
+        }
+ 
+        wrapSelectedText(` [${descriptionPlaceholder}](${protocol}`, ') ', linkPlaceholder);
     }
 
     return (
@@ -48,11 +95,11 @@ function TextEditor(props) {
             <div className="markdown-control-panel">
                 <div className="text-type">
                     <i className="fa fa-bold" onClick={handleBoldIconClick}></i>
-                    <i className="fa fa-italic"></i>
+                    <i className="fa fa-italic" onClick={handleItalicIconClick}></i>
                 </div>
 
                 <div className="text-format">
-                    <i className="fa fa-link"></i>
+                    <i className="fa fa-link" onClick={handleHyperlinkIconClick}></i>
                     <i className="fa fa-image"></i>
                 </div>
             </div>
@@ -64,7 +111,6 @@ function TextEditor(props) {
                     name={props.name}
                     value={content}
                     onChange={handleContentChange}
-                    onMouseUp={handleTextareaMouseUp}
                 ></textarea>
                 <div className="markdown-result">
                     <ReactMarkdown 
